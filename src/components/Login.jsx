d548529ed574f74ebc8cb8060bd91f3a620e40a5
Lakshay <nijhawanlakshay06@gmail.com>
2024-04-02 11:01:53 +0530

    Changed nomenclature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -36,7 +36,7 @@ function Login() {
                         <Logo width="100%" />
                     </span>
         </div>
-        <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+        <h2 className="text-center text-2xl font-bold leading-tight">Login to your account</h2>
         
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
         <form onSubmit={handleSubmit(login)} className='mt-8'>
@@ -64,7 +64,7 @@ function Login() {
                 <Button
                 type="submit"
                 className="w-full"
-                >Sign in</Button>
+                >Login</Button>
             </div>
             <p className="mt-2 text-center text-base text-black/60">
                     Don&apos;t have an account?&nbsp;
